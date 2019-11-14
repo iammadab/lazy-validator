5b5c9d31e32ece98bf8e4bb397b659246824a8e8
iammadab <iammadab@gmail.com>
2019-11-14 14:28:47 +0100

    Moved the new version to the main index file

--- index2.js
+++ index2.js
@@ -1,52 +1,72 @@
-const { parseInput } = require("./util")
-const createResolver = require("./baseFunctions")
+const typeMap = {
+	"String": String,
+	"Number": Number,
+	"Boolean": Boolean,
+	"Array": Array,
+	"Object": Object,
+	"Function": Function
+}
 
-function createValidator(validationString){
-	let instructionObj = parseInput(validationString)
-	let resolver = createResolver()
-
-	let parser = createParser(instructionObj, resolver)
-
-	return {
-		parse: parser,
-		add: resolver.add
-	}
+const functionMap = {
 
 }
 
-function createParser(instructionObj, resolver){
-	return function parse(objToValidate){
-		let instructionProps = Object.keys(instructionObj), parserResult = {}
-
-		instructionProps.forEach(currentProperty => {
-			let exists = resolver.resolve("hasproperty")(objToValidate, currentProperty)
-			if(exists.error)
-				return parserResult[currentProperty] = exists
-
-			let transforms = instructionObj[currentProperty], currentValue = objToValidate[currentProperty]	
-
-			applyTransformations(transforms, currentProperty, currentValue, resolver, parserResult)
+function createValidator(validationRule){
+	let syncValidator = createValidatorSync(validationRule)
+	return function (objectToValidate){
+		return new Promise(function(resolve, reject){
+			let result = syncValidator(objectToValidate)
+			resolve(result)
 		})
+	}
+}
+
+function createValidatorSync(validationRule){
+	if(removeSpaces(validationRule) == "") return dumbValidator
+
+	return function (objectToValidate){
+		let rules = grabRules(removeSpaces(validationRule)), properties = grabProperties(rules), types = grabTypes(rules), errors = []
+
+		types = types.map(capitalizeFirst)
+
+		let propertyErrors = verifyAllProperties(properties, objectToValidate)
+		let typeErrors = verifyAllType(properties, types, objectToValidate)
+
+		errors = errors.concat(propertyErrors, typeErrors)
 		
-		return parserResult
+		if(errors.length > 0)
+			return { error: true, errors: errors }
+		else
+			return { error: false, errors: errors }
 	}
 }
 
-function applyTransformations(transforms, currentProperty, currentValue, resolver, resultObj){
-	for(let i = 0; i < transforms.length; i++){
-		let transform = transforms[i], transformFunction = resolver.resolve(transform)
+function verifyAllProperties(properties, obj){
+	let errors = []
+	properties.forEach(property => {
+		let error = verifyProperty(property, obj)
+		errors = errors.concat(error)
+	})
+	return errors
+}
 
-		if(transformFunction.error)
-			return resultObj[currentProperty] = transformFunction
-
-		let transformationResult = transformFunction(currentProperty, currentValue)
-
-		if(transformationResult.error)
-			return resultObj[currentProperty] = transformationResult
-
-		currentValue = transformationResult.data
+function verifyAllType(properties, types, values){
+	let errors = []
+	for(let i = 0; i < properties.length; i++){
+		let error = verifyType(types[i], properties[i], values[properties[i]])
+		errors = errors.concat(error)
 	}
-	resultObj[currentProperty] = currentValue
+	return errors
+}
+
+function dumbValidator(objectToValidate){
+	return { error: false, errors: [] }
+}
+
+
+module.exports = {
+	createValidator: createValidator,
+	createValidatorSync: createValidatorSync
 }
 
 
@@ -61,11 +81,78 @@ function applyTransformations(transforms, currentProperty, currentValue, resolve
 
 
 
-// let myVal = createValidator("username.string.lowercase, password.string.addSome.lowercase")
-// myVal.add("lowercase", function(name, value){
-// 	return { error: false, data: value.toLowerCase() }
-// })
-// myVal.add("addSome", function(name, value){
-// 	return { error: false, data: value += "blahBLAH" }
-// })
-// console.log(myVal.parse({username: "Wisdom", password: "NicEstuff"}))
+
+
+
+
+
+
+//Helpers
+
+function removeSpaces(string){
+	return string.replace(/\s/g, "")
+}
+
+function grabRules(ruleString){
+	return ruleString.split(",")
+}
+
+function grabProperties(rules){
+	return rules.map(function(rule){
+		return rule.split(".")[0]
+	})
+}
+
+function grabTypes(rules){
+	return rules.map(function(rule){
+		return rule.split(".")[1]
+	})
+}
+
+function verifyType(typename, propertyName, value){
+	let errors = []
+	let typeFn = typeMap[typename]
+
+	if(!typeFn)
+		errors.push({ error: "Invalid Type Function", message: `(${typename}) is not a valid type`})
+
+	else if(typename == "Number")
+		if(!isNumber(value))
+			errors.push({ error: "Type Error", message: `Expected (${typename}) for (${propertyName}) instead got (${typeof value})`})
+
+	else if(typename == "Boolean")
+		if(!isBoolean(value))
+			errors.push({ error: "Type Error", message: `Expected (${typename}) for (${propertyName}) instead got (${typeof value})`})
+
+	else if(typeof typeFn() != typeof value)
+		errors.push({ error: "Type Error", message: `Expected (${typename}) for (${propertyName}) instead got (${typeof value})`})
+
+	return errors
+
+
+	// Small helpers
+	function isNumber(value){
+		if(isNaN(+value) || typeof value == "boolean")
+			return false
+		return true
+	}
+
+	function isBoolean(value){
+		let lowCaseValue = ("" + value).toLowerCase(), acceptable = ["true", "false"]
+		if(!acceptable.includes(lowCaseValue))
+			return false
+		return true
+	}
+}
+
+function verifyProperty(propertyName, obj){
+	let errors = []
+	if(!obj.hasOwnProperty(propertyName))
+		errors.push({ error: "Property not found", message: `Expected property called (${propertyName})`})
+	return errors
+}
+
+function capitalizeFirst(string){
+	lowerCaseValue = string.toLowerCase()
+	return lowerCaseValue.charAt(0).toUpperCase() + lowerCaseValue.slice(1)
+}
