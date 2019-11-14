2d0480733ebdfdce7a94a77792eb2de7178150d0
iammadab <iammadab@gmail.com>
2019-11-14 13:23:31 +0100

    Tested the resolver logic

--- index2.js
+++ index2.js
@@ -1,14 +1,6 @@
 const { parseInput } = require("./util")
 const createResolver = require("./baseFunctions")
 
-let resolver = createResolver()
-console.log(resolver)
-
-console.log(resolver.resolve("number")("5035"))
-console.log(resolver.resolve("email"))
-resolver.add("email", val => val.indexOf("@") > 0)
-console.log(resolver.resolve("email"))
-
 function createValidator(validationString){
 
 	let instructionObj = parseInput(validationString)
