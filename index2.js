4ed587e63702d5508d88f4f0c5929ebf1db49980
iammadab <iammadab@gmail.com>
2019-11-14 14:26:27 +0100

    Cleaned up the new code

--- index2.js
+++ index2.js
@@ -2,11 +2,20 @@ const { parseInput } = require("./util")
 const createResolver = require("./baseFunctions")
 
 function createValidator(validationString){
-
 	let instructionObj = parseInput(validationString)
 	let resolver = createResolver()
 
-	function parse(objToValidate){
+	let parser = createParser(instructionObj, resolver)
+
+	return {
+		parse: parser,
+		add: resolver.add
+	}
+
+}
+
+function createParser(instructionObj, resolver){
+	return function parse(objToValidate){
 		let instructionProps = Object.keys(instructionObj), parserResult = {}
 
 		instructionProps.forEach(currentProperty => {
@@ -21,22 +30,7 @@ function createValidator(validationString){
 		
 		return parserResult
 	}
-
-	return {
-		parse,
-		add: resolver.add
-	}
-
 }
-
-let myVal = createValidator("username.string.lowercase, password.string.addSome.lowercase")
-myVal.add("lowercase", function(name, value){
-	return { error: false, data: value.toLowerCase() }
-})
-myVal.add("addSome", function(name, value){
-	return { error: false, data: value += "blahBLAH" }
-})
-console.log(myVal.parse({username: "Wisdom", password: "NicEstuff"}))
 
 function applyTransformations(transforms, currentProperty, currentValue, resolver, resultObj){
 	for(let i = 0; i < transforms.length; i++){
@@ -54,3 +48,24 @@ function applyTransformations(transforms, currentProperty, currentValue, resolve
 	}
 	resultObj[currentProperty] = currentValue
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let myVal = createValidator("username.string.lowercase, password.string.addSome.lowercase")
+// myVal.add("lowercase", function(name, value){
+// 	return { error: false, data: value.toLowerCase() }
+// })
+// myVal.add("addSome", function(name, value){
+// 	return { error: false, data: value += "blahBLAH" }
+// })
+// console.log(myVal.parse({username: "Wisdom", password: "NicEstuff"}))
